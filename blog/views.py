d77f752462fbc29e5fc49d20821207a7a39fa709
drsiyarb <siyarbahadir@gmail.com>
2015-12-13 12:46:05 +0200

    html içine django yardımıyla python kodu yerleştirdim

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	return render(request, 'blog/post_list.html', {'posts': posts})
 # Create your views here.
